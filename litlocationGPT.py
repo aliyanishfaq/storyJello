eb80854941fecec17b4154dfbfa3bbc4759d1ccc
Aliyan Ishfaq <aliyanishfaq200@gmail.com>
2024-02-13 19:54:23 -0800

    Update litlocationGPT.py

--- litlocationGPT.py
+++ litlocationGPT.py
@@ -5,7 +5,7 @@ def gptResponse(messages):
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=messages,
-    api_key='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'
+    api_key='{YOUR API KEY}'
     )
     print(response)
     return response['choices'][-1]['message']['content']
